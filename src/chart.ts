f644dda0144cf5900f393324deda496ba2535086
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-10-02 23:33:52 +0300

    added chart functions

---
+++ src/chart.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { items, chart } from "./data";
+
+export interface ChartItem {
+  id: number;
+  count: number
+}
+
+export const getAllItemsInChart = (_request: Request, res: Response): void => {
+  res.status(200).send(chart);
+};
+
+export const addItemToChart = (request: Request, res: Response): void => {
+  const id = +request.params.id;
+  const item = items.find(item => item.id === id);
+  
+  // TODO: immutability when finished!
+  chart.some(chartItem => item.id === chartItem.id) ? chart.find(chartItem => item.id === chartItem.id) :
+    chart.push({id: item.id, count: 1});
+
+  item ? res.status(200).send("Success!") : res.status(404).send(`Item with id ${id} does not exist`);
+};
