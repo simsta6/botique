8e4bff8e6a2fe4113b4d31653dd7ef16e4e2e646
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-10-02 23:47:43 +0300

    added seller functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express from "express";
-import { getAllItems } from "./items";
+import { getAllItems, getFiltratedItems } from "./item";
+import { getAllItemsInChart, addItemToChart } from "./chart";
+import { makeAnOrder } from "./order";
+import { addItem } from "./seller";
 
 const server = express();
 const port = 5000;
 
+// Item
 server.get("/api/items", getAllItems);
+server.get("/api/items/:color", getFiltratedItems);
+
+// Chart
+server.get("/api/chart", getAllItemsInChart);
+server.post("/api/chart-add/:id", addItemToChart);
+
+// Order
+server.post("/api/make-order", makeAnOrder);
+
+// Seller
+server.post("/api/add-item", addItem);
+
 
 server.listen(port, () => console.log(`Running on port ${port}`));
