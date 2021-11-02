27d378c3f81b90b2e8cf80c6d5ca366b99fde425
simsta6 <51540204+simsta6@users.noreply.github.com>
2021-11-02 13:55:57 +0200

    Changes

--- dist/config/database.js
+++ dist/config/database.js
@@ -5,11 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connect = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-const connectionUri_1 = require("../connectionUri");
 const connect = () => {
-    // Connecting to the database
-    mongoose_1.default
-        .connect(connectionUri_1.mongoDBUri)
+    mongoose_1.default.connect(process.env.MONGO_URI)
         .then(() => {
         console.log("Successfully connected to database");
     })
--- dist/connectionUri.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongoDBUri = void 0;
-exports.mongoDBUri = "mongodb+srv://admin:admin@cluster0.bmoxo.mongodb.net/botique?retryWrites=true&w=majority";
-//# sourceMappingURL=connectionUri.js.map
